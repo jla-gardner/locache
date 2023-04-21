4aecb2752c3a1a44d22f2ff1d4f128e9c44f47a3
John Gardner <gardner.john97+dphil@gmail.com>
2023-04-21 14:46:40 +0100

    rewrite

--- locache/__init__.py
+++ locache/__init__.py
@@ -1,23 +1,23 @@
-import functools
-import warnings
-from typing import Callable
+import inspect
+import logging
+import pickle
+from functools import partial
+from hashlib import sha256
+from pathlib import Path
 
-from locache.base import LocalCache, get_cache_location, reset
-from locache.logging import verbose
-from locache.util import RUNNING_PATH
+__all__ = ["persist", "verbose"]
+__version__ = "2.1.1"
 
 
-def persist(function=None, *, name: str = None, auto_invalidate=True):
+def persist(func=None, *, auto_invalidate=True):
     """
     decorator for caching function results to disk
 
     If the function bar is defined in foo.py, then the results
-    will be stored in ./foo.cache/bar/, unless a name is specified,
-    in which case the results will be stored in ./name.cache/foo/.
+    will be stored in ./foo.cache/bar/
 
     Args:
         function: the function to cache
-        name: the name of the cache (if not specified, the function name is used)
         auto_invalidate: if True, the cache will be invalidated if the function code changes
 
     Returns:
@@ -28,38 +28,82 @@ def persist(function=None, *, name: str = None, auto_invalidate=True):
         def add(a, b):
             return a + b
 
-        @persist(name="addition")
+        @persist(auto_invalidate=False)
         def add(a, b, c):
             return a + b + c
     """
-
-    def persist_decorator(func):
-        location = get_cache_location(func, name)
-        cache = LocalCache(func, location, auto_invalidate)
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            return cache.call(*args, **kwargs)
-
-        return wrapper
-
-    if function == None:  # called as @persist(name=...)
-        return persist_decorator
-    else:  # called as @persist
-        return persist_decorator(function)
-
-
-def reset_cache(func: Callable = None, name: str = None) -> None:
-    """
-    reset the cache for a function
-    """
-
     if func is None:
-        location = RUNNING_PATH / f"{name}.cache"
-    else:
-        location = get_cache_location(func, name)
+        return partial(persist, auto_invalidate=auto_invalidate)
 
-    if location.exists():
-        reset(location)
+    location = get_filename_containing_(func)
+    cache = LocalCache(func, location, auto_invalidate)
+    return cache
+
+
+_logger = logging.getLogger(f"{__name__} : local_cache")
+_logger.setLevel(logging.INFO)
+_logger.addHandler(logging.StreamHandler())
+
+
+def verbose(val: bool):
+    if val:
+        _logger.setLevel(logging.DEBUG)
     else:
-        warnings.warn(f"Attempted to reset cache at {location}, but it does not exist")
+        _logger.setLevel(logging.INFO)
+
+
+class LocalCache:
+    def __init__(self, func, location, auto_invalidate=False):
+        self.func = func
+        self.backend = Backend(func, location, auto_invalidate)
+        self.__doc__ = func.__doc__
+
+    def __call__(self, *args, **kwargs):
+        cache_path = self.backend.get_cache_path(*args, **kwargs)
+        _logger.debug(f"Querying cache at {self.backend.location} for {args}, {kwargs}")
+
+        if cache_path.exists():
+            _logger.debug("Cache hit")
+            with open(cache_path, "rb") as f:
+                return pickle.load(f)
+        else:
+            _logger.debug("Cache miss")
+            result = self.func(*args, **kwargs)
+            with open(cache_path, "wb") as f:
+                pickle.dump(result, f)
+            return result
+
+    def __repr__(self):
+        n = len(list(self.backend.location.glob("*.pkl")))
+        return f"<cache for {self.func.__name__} with {n} entries>"
+
+
+class Backend:
+    def __init__(self, func, location, auto_invalidate):
+        self.location = location
+        code_file = location / ".code.py"
+        new_code = inspect.getsource(func)
+
+        if not location.exists() or not code_file.exists():
+            location.mkdir(parents=True, exist_ok=True)
+            code_file.write_text(new_code)
+            return
+
+        old_code = code_file.read_text()
+        if auto_invalidate and new_code != old_code:
+            _logger.info(f"Detected code change for {func.__name__}. Deleting cache.")
+            self.location.rmdir()
+            self.location.mkdir(parents=True)
+            code_file.write_text(new_code)
+
+    def get_cache_path(self, *args, **kwargs):
+        dump = pickle.dumps((args, kwargs))
+        signature = sha256(dump).hexdigest()[:32]
+        return self.location / f"{signature}.pkl"
+
+
+def get_filename_containing_(func):
+    path = Path(inspect.getfile(func))
+    if "ipykernel" in str(path):
+        path = Path.cwd() / "notebook.py"
+    return path.with_suffix(".cache") / func.__name__
